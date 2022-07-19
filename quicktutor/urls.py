d5b31e234803c04f61897fb4a7eaa061e0c86b6d
NessajUrsa2518 <nessaj.ursa@gmail.com>
2022-07-19 22:59:55 +0800

    Update navbar with account menu

--- quicktutor/urls.py
+++ quicktutor/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from home import views
 from accounts import views as accounts_views
@@ -24,4 +25,5 @@ urlpatterns = [
 
     url(r'^$', views.home, name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
