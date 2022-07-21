d52db83036d0700e8bf8aca010526289340577f5
NessajUrsa2518 <nessaj.ursa@gmail.com>
2022-07-21 20:36:23 +0800

    Change test code for form tag

--- home/tests.py
+++ home/tests.py
@@ -19,8 +19,4 @@ class HomeTests(TestCase):
 
 	def test_csrf(self):
 		self.assertContains(self.response, 'csrfmiddlewaretoken')
-	
-	def test_contains_form(self):
-		form = self.response.context.get('form')
-		self.assertIsInstance(form, SignUpForm)
 
