8573a9034a16efb6e204779ec1e56dce1f16d767
NessajUrsa2518 <nessaj.ursa@gmail.com>
2022-07-21 10:34:41 +0800

    change seetings.py

--- quicktutor/settings.py
+++ quicktutor/settings.py
@@ -137,4 +137,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "nessaj.ursa@gmail.com"
-EMAIL_HOST_PASSWORD = "K(j?'_=G%VbE#3"
+EMAIL_HOST_PASSWORD = "mypassword"
